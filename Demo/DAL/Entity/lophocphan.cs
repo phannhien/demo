e33d277c70582ddbacd6229f1596725980f373a3
phannhien <phanhien1234@gmail.com>
2019-12-22 10:13:35 +0700

    first commit

---
+++ Demo/DAL/Entity/lophocphan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entity
+{
+    public class lophocphan
+    {
+        public int malophocphan { get; set; }
+        public string tenlophocphan { get; set; }
+        public string monhoc { get; set; }
+        public string giaovien { get; set; }
+        public int slsvdangky { get; set; }
+        public int slsvmax { get; set; }
+    }
+}
---
+++ Demo/DAL/Entity/sinhvien.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entity
+{
+    public class sinhvien
+    {
+        public int masinhvien { get; set; }
+        public string hodem { get; set; }
+        public string ten { get; set; }
+        public DateTime ngaysinh { get; set; }
+        public string quequan { get; set; }
+        public int malophocphan { get; set; }
+    }
+}
